d5822ac3135842346b7607798917d3c42f32cd48
athoub <athoub2004@gmail.com>
2020-07-01 16:57:14 +0300

    dn

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,11 +1,16 @@
 let instructor = {
   name: "Menawer",
   courses: ["JavaScript", "React", "PHP"],
-  githubAccount: {
+  githubAccount: 
+  {
     username: "careless10",
     numberOfRepos: 200,
+    
+    
   },
   sayHello: function () {
     console.log("Hi, I'm Menawer");
+
+  console.log(instructor);
   },
 };
